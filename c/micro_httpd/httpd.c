d0356dfa2dde49973b0664bdf149a693ed7a57a1
CaoYY <bryantly@126.com>
2016-05-30 00:23:12 +0800

    micro_httpd request header parser

--- c/micro_httpd/httpd.c
+++ c/micro_httpd/httpd.c
@@ -7,6 +7,7 @@
 #include <error.h>
 #include <pthread.h>
 #include <regex.h>
+#include <stdbool.h>
 #include "httpd.h"
 
 
@@ -42,6 +43,55 @@ int server_setup(unsigned int addr, unsigned short port) {
     }
 
     return socket_fd;
+}
+
+int request_parser(int fd, char *header, size_t header_size, char *body, size_t body_size) {
+    size_t read_size, body_read_size = 0;
+    int ret;
+    char body_buffer[body_size];
+    regex_t regex;
+    regmatch_t regmatch[1];
+
+    read_size = read(fd, header, header_size);
+    if (read_size < 0) {
+        return -1;
+    }
+
+    ret = regcomp(&regex, "\r\n\r\n", REG_EXTENDED);
+    if (ret) {
+        DEBUG_PRINT("read regex compile ret -> %d\n", ret);
+        return -1;
+    }
+
+    ret = regexec(&regex, header, 1, regmatch, 0);
+    if (ret == 0) {
+        // found header end flag \r\n\r\n in header string
+        body_read_size = read_size - regmatch[0].rm_eo;
+        strncpy(body, header[regmatch[0].rm_eo], body_read_size);
+        header[regmatch[0].rm_so] = '\0';
+        if (read_size == header_size) {
+            read(fd, body[body_read_size + 1], body_size - body_read_size - 1);
+        }
+    }
+    else {
+        read_size = read(fd, body_buffer, body_size);
+        if (read_size < 0) {
+            return -1;
+        }
+        ret = regexec(&regex, body_buffer, 1, regmatch, 0);
+        if (ret != 0) {
+            // not end of heder yet
+            return -1;
+        }
+        body_read_size = read_size - regmatch[0].rm_eo;
+        strncpy(body, body_buffer[regmatch[0].rm_eo], body_read_size);
+        if (read_size == body_size) {
+            read(fd, body[body_read_size + 1], body_size - body_read_size - 1);
+        }
+    }
+
+    regfree(&regex);
+    return 0;
 }
 
 int read_util_regex(int fd, const char *preg, char *buffer, int max_size) {
@@ -80,19 +130,145 @@ int read_util_regex(int fd, const char *preg, char *buffer, int max_size) {
     return total_read;
 }
 
+int header_parser(const char *header, http_header headers) {
+    char prev_ch, ch;
+    char line[MAX_HEADER_SIZE];
+    bool first = true;
+
+    int i=0;
+    int line_pos = 0;
+    for(;;) {
+        if (i == 0) {
+            prev_ch = ch = header[i];
+        }else {
+            prev_ch = header[i-1];
+            ch = header[i];
+        }
+
+        if ((prev_ch == '\r' && ch == '\n') || ch == '\0') {
+            line[line_pos] = '\0';
+            // line parser
+            DEBUG_PRINT("get line -> %s\n", line);
+            header_line_parser(line, &headers, first);
+            line_pos = 0;
+            first = false;
+        }
+        else {
+            line[line_pos] = header[i];
+            line_pos++;
+        }
+
+        if (ch == '\0') {
+            break;
+        }
+        i++;
+    }
+
+
+}
+
+void header_line_parser(const char *line, http_header *headers, bool first) {
+    char buffer[1024];
+    int segment = 0;
+    int i = 0;
+    int pos = 0;
+    if (first) {
+        for(;;) {
+            if (line[i] == ' ' || line[i] == '\0') {
+                buffer[pos] = '\0';
+                DEBUG_PRINT("line parser -> %s\n", buffer);
+                switch(segment) {
+                    case 0:
+                        strcpy(headers->method, buffer);
+                        break;
+                    case 1:
+                        strcpy(headers->path, buffer);
+                        break;
+                    case 2:
+                        strcpy(headers->protocol, buffer);
+                        break;
+                }
+                pos = 0;
+                segment++;
+            }
+            else {
+                buffer[pos] = line[i];
+                pos++;
+            }
+
+            if (line[i] == '\0') {
+                break;
+            }
+            i++;
+        }
+
+        DEBUG_PRINT("method: %s, path: %s, protocol: %s\n", headers->method, headers->path, headers->protocol);
+    }
+    else {
+        char key[1024];
+        for(;;) {
+            if (segment == 0 && line[i] == ':') {
+                buffer[pos] = '\0';
+                strcpy(key, buffer);
+
+                // skip space
+                pos = 0;
+                segment = 1;
+                i++;
+            }
+            else {
+                buffer[pos] = line[i];
+                pos++;
+            }
+
+            if (line[i] == '\0') {
+                break;
+            }
+            i++;
+        }
+
+        if (strcmp(key, "Host") == 0) {
+            DEBUG_PRINT("host -> %s\n", buffer);
+            strcpy(headers->host, buffer);
+        }
+        else if (strcmp(key, "User-Agent") == 0) {
+            DEBUG_PRINT("user_agent: %s\n", buffer);
+            strcpy(headers->user_agent, buffer);
+        }
+        else if (strcmp(key, "Accept") == 0) {
+            DEBUG_PRINT("accept: %s\n", buffer);
+            strcpy(headers->accept, buffer);
+        }
+    }
+}
+
+
 void accept_request(void *connect) {
     int connect_sock = *(int *)connect;
+    int ret;
     pthread_t tid = pthread_self();
     DEBUG_PRINT("Thread id %d serve for socket %d.\n", (long)tid, connect_sock);
 
     int fflag = fcntl(connect_sock, F_GETFL, 0);
     fcntl(connect_sock, F_SETFL, fflag | O_NONBLOCK);
 
-    char header_buffer[MAX_HEADER_SIZE];
-    read_util_regex(connect_sock, "\r\n\r\n", header_buffer, 5);
+    char header[MAX_HEADER_SIZE];
+    char body[MAX_BODY_SIZE];
+    ret = request_parser(connect_sock, header, MAX_HEADER_SIZE, body, MAX_BODY_SIZE);
+    if (ret != 0) {
+        DEBUG_PRINT("return ret %d from request_parser.", ret);
+        return;
+    }
 
-    fprintf(stdout, "header:\n");
-    fprintf(stdout, header_buffer);
+    fprintf(stdout, "header: -> \n");
+    fprintf(stdout, header);
+    fprintf(stdout, "\n");
+    fprintf(stdout, "body: -> \n");
+    fprintf(stdout, body);
+    fprintf(stdout, "\n");
+
+    http_header headers;
+    header_parser(header, headers);
 
     fflush(stdout);
     close(connect_sock);
--- c/micro_httpd/httpd.h
+++ c/micro_httpd/httpd.h
@@ -8,9 +8,27 @@
     do { if (DEBUG) fprintf(stderr, fmt, __VA_ARGS__); } while (0)
 
 
+// header data struct
+typedef struct header_pair {
+    char *key;
+    char *value;
+} header_pair;
+
+typedef struct http_header {
+    char method[1024];
+    char path[1024];
+    char protocol[1024];
+    char version[1024];
+    char host[1024];
+    char user_agent[1024];
+    char accept[1024];
+} http_header;
+
+
 void error_exit(char *);
 void accept_request(void *);
 
 int read_util_regex(int fd, const char *preg, char *buffer, int max_size);
+int request_parser(int fd, char *header, size_t header_size, char *body, size_t body_size);
 
 int server_setup(unsigned int addr, unsigned short port);
